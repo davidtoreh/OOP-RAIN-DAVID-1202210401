a5b133d88fd43983d3b153d19323da015728c168
davidtoreh <91960274+davidtoreh@users.noreply.github.com>
2022-10-31 12:02:23 +0700

    Delete MODUL 1 directory

--- MODUL 1/src/User.java
+++
@@ -1,6 +0,0 @@
-public class User {
-    public static void main(String[] args){
-        System.out.println("Nama = David_SI4506");
-        System.out.println("No. Handphone = 1202210401");
-    }
-}
